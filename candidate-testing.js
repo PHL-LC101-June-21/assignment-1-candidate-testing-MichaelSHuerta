e76b201338a5846fd1decb76cdf9938134473cb9
MichaelHuerta <85526782+MichaelSHuerta@users.noreply.github.com>
2021-06-14 20:34:45 +0000

    I  used input.question to enter the named. used  == for true and false

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,8 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
 let questions;
 let correctAnswers;
@@ -15,11 +15,14 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
-}
+  candidateName = input.question("enter name:");
+  console.log(" Hello " + candidateName )
+} 
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  candidateAnswer = input.question( question )
+
 
 
 }
@@ -27,7 +30,12 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer == correctAnswer){
+  console.log( "you are correct")
+  }
+  else {
+    console.log("you are wrong")
+  }
 
   let grade;
   
