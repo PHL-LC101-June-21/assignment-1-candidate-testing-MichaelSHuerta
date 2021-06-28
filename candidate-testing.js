54afc30539c5e4ea955797e2c743b9f210452ae5
MichaelHuerta <85526782+MichaelSHuerta@users.noreply.github.com>
2021-06-28 07:25:01 +0000

    This is a new version

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,9 @@ let candidateName;
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride","true", "40","Trajectory","3"]
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -21,23 +21,58 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question( question )
-
+  for(i=0;i<questions.length;i++){
+    answers = input.question(questions[i])
+     candidateAnswers.push(answers)
+  }
 
 
 }
 
 function gradeQuiz(candidateAnswers) {
+  let grade = 0;
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer){
-  console.log( "you are correct")
+for(i=0;i<questions.length;i++){
+if (candidateAnswers[i] === correctAnswers[i]){
+  console.log("Youre correct")
+  grade++
   }
   else {
-    console.log("you are wrong")
+    console.log("Your Answer:" + candidateAnswers[i])
+    console.log("Correct Answer:" + correctAnswers[i])
+  }
+}
+
+
+
+
+  
+  //Number of Correct Answers) / (Number of Quiz Questions) * 100
+  let total = (grade/correctAnswers.length) * 100
+  console.log( ">>> Overall Grade: " + total + "% " + grade + " out of 5 responses correct) <<<")
+  
+
+  if(total >= 60){
+    console.log (">>> Status: PASSED <<<")
+  }
+    else {
+      console.log(">>> Status: FAILED <<<")
+    
   }
 
-  let grade;
+
+//   while(i < correctAnswers.length) {
+// if (candidateQuizAnswer[i] === correctAnswer[i]){
+// candidateAnswers = candidateAnswers +1;
+// } i++
+// }
+  
+// let percentage = (scoreCounter)/(answerKey.length)*100
+// console.log(>>> Overall Grade: ${percentage}% (${scoreCounter} of 5 responses correct) <<<);
+
+
+
   
 
   return grade;
@@ -50,6 +85,43 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
